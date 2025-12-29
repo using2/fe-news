cac4c07daa323f45f32116b25756b881a2a4d9cf
apple <apple@appleui-MacBookPro.local>
2025-12-29 17:40:23 +0900

    feat: html 초안 작성

---
+++ src/main.js
@@ -0,0 +1,58 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <header>
+      <h1><p>뉴스스탠드</p></h1>
+    </header>
+    <section>
+      <div> 
+        <span>연합뉴스</span>
+        <span>[속보] 도심 공원 '조용한 독서존' 시범운영... 시민 호응 이어져</span>
+      </div>
+      <div> 
+        <span>연합뉴스</span>
+        <span>[속보] 도심 공원 '조용한 독서존' 시범운영... 시민 호응 이어져</span>
+      </div>
+    </section>
+    <section>
+      <table>
+        <tr> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+        </tr>
+        <tr> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+        </tr>
+        <tr> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+        </tr>
+        <tr> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+        </tr>
+        <tr> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+          <td>서울경제</td> 
+        </tr>
+      </table>
+    </section>
+  </div>
+`
