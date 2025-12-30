e7e447d2d893bb27cd652325adc0b614f2a9d1f6
apple <apple@appleui-MacBookPro.local>
2025-12-30 18:04:07 +0900

    feat: page load to main

--- src/main.js
+++ src/main.js
@@ -1,65 +1,10 @@
-import './style.css'
+import newsstand from "./pages/newsstand.js";
+import { initNewsFeed } from "./components/newsFeed.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <header>
-      <h1><p>뉴스스탠드</p></h1>
-      <p>2025.12.29. 월요일</p> 
-    </header>
-    <section>
-      <div> 
-        <span>연합뉴스</span>
-        <span>[속보] 도심 공원 '조용한 독서존' 시범운영... 시민 호응 이어져</span>
-      </div>
-      <div> 
-        <span>연합뉴스</span>
-        <span>[속보] 도심 공원 '조용한 독서존' 시범운영... 시민 호응 이어져</span>
-      </div>
-    </section>
-    <section>
-      <section>
-        <div>
-          <button><span>전체 언론사</span></button>
-          <button><span>내가 구독한 언론사</span><span>0</span></button>
-        </div>
-      </section>
-      <table>
-        <tr> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-        </tr>
-        <tr> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-        </tr>
-        <tr> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-        </tr>
-        <tr> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-        </tr>
-        <tr> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-          <td>서울경제</td> 
-        </tr>
-      </table>
-    </section>
-  </div>
-`
+const app = document.querySelector("#app");
+app.innerHTML = newsstand();
+
+const newsFeedContainer = app.querySelector(".news-feed-container");
+if (newsFeedContainer) {
+  initNewsFeed(newsFeedContainer);
+}
